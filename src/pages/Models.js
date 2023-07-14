7ba61f88b35befcedbe4877c4969fdb7c83a99a0
yodit93 <yoditabebe9391@gmail.com>
2023-07-14 15:42:33 +0300

    Create model component

--- src/pages/Models.js
+++ src/pages/Models.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CarModel from '../components/CarModel';
 
-const Models = () => (
-  <div>Home Page</div>
-);
+const Cars = () => {
+  const [cars, setCars] = useState([]);
 
-export default Models;
+  useEffect(() => {
+    const url = 'http://127.0.0.1:3001/api/v1/cars';
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setCars(data));
+  }, []);
+
+  return (
+    <div className='container d-flex flex-row'>
+      {cars.map((car) => (
+        <CarModel car={car} key={car.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Cars;
