22952bb0475cb771213b269502551f9cf5176b9a
yodit93 <yoditabebe9391@gmail.com>
2023-07-16 16:28:46 +0300

    Create redux store for users

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import carsReducer from './carsSlice';
+import usersReducer from './userSlice';
 
 const store = configureStore({
   reducer: {
+    users: usersReducer,
     cars: carsReducer,
   },
 });
