71ed9499e242005f003015c81cb5cd2b9466718d
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-07-17 14:25:44 +0300

    create dispatch action method

--- src/pages/MyReservations.js
+++ src/pages/MyReservations.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateReservation,createReservation, deleteReservation  } from '../Redux/reservationsSlice';
+
 
 const MyReservations = () => (
-  <div>My Reservations Page</div>
+  const dispatch = useDispatch();
+  const reservations = useSelector((state) => reservations.reservations)
+  useEffect(()=> {
+    dispatch(updateReservation())
+  }, [dispatch]);
+
+
+  <div className='reservation_main_container'>
+    <NavigationPanel/>
+    <div className='reservation_container'>
+      <h2>My Reservations</h2>
+    </div>
+
+  </div>
 );
 
 export default MyReservations;
