a3ef9841896a399dbed54f87b96f60b619be74b5
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-07-18 13:22:06 +0300

    render car name and dispatch action get cars

--- src/pages/MyReservations.js
+++ src/pages/MyReservations.js
@@ -1,29 +1,36 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getReservations, deleteReservation } from '../Redux/reservationsSlice';
+import { getCars } from '../Redux/carsSlice';
 import NavigationPanel from '../Components/Navigation/NavigationPanel';
 import '../Styles/myreservations.css';
 
 const MyReservations = () => {
   const dispatch = useDispatch();
   const reservations = useSelector((state) => state.reservations.reservations);
+  const cars = useSelector((state) => state.cars.cars);
 
   useEffect(() => {
     dispatch(getReservations());
+    dispatch(getCars());
   }, [dispatch]);
 
   const handleDelete = (id) => {
     const confirmDelete = window.confirm('Are you sure you want to delete this Reservation?');
     if (confirmDelete) {
       dispatch(deleteReservation(id));
-      dispatch(deleteReservation(id));
     }
+  };
+
+  const getCarName = (carId) => {
+    const car = cars.find((car) => car.id === carId);
+    return car ? car.name : 'Unknown Car';
   };
 
   return (
     <div className="reservation_main_container">
       <NavigationPanel />
-      <div className="reservation_container">
+      <div className="reservation_container" id="add-car-overlay">
         <h2 className="reservations_header">
           My Reservations
         </h2>
@@ -32,7 +39,7 @@ const MyReservations = () => {
             <div key={reservation.id}>
               <div className="card_container">
                 <div className="card_wrapper">
-                  <h2>{reservation.car}</h2>
+                  <h2 className="card_detail">{getCarName(reservation.car_id)}</h2>
                   <p className="card_detail">
                     city:
                     {reservation.city}
