cfa65a3fde93d35ffd5ab04ee00143e6b0e69462
Juwon <hardejuwon@gmail.com>
2023-07-25 12:15:23 +0100

    finished details page styling

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,70 +1,91 @@
 import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { BiChevronRight } from 'react-icons/bi';
+import { CiSettings } from 'react-icons/ci';
+import multicolor from './images/multicolor.png';
+
 import { fetchCarDetails } from '../Redux/carsSlice';
 import NavigationPanel from '../Components/Navigation/NavigationPanel';
 import '../Styles/details.css';
 
 const DetailsPage = () => {
+  const navigate = useNavigate()
   const { id } = useParams();
   const dispatch = useDispatch();
-  const { cardetails, isLoading } = useSelector((store) => store.cars);
-
+  const { cardetails } = useSelector((store) => store.cars);
+  const handleClick = () => {
+navigate('/')
+  };
   useEffect(() => {
     dispatch(fetchCarDetails(id));
   }, [dispatch]);
-
+  console.log(cardetails);
   return (
-    <div className="details-page">
+    <div className="cunt">
       <NavigationPanel />
-      <div className="details-image-container">
-        {(
-          cardetails.map((element) => (
-            <div className="thecar"><img key={element.id} src={element.image} alt={element.model} className="carimage" /></div>
-          ))
-        )}
-      </div>
-      <div className="details-content-container">
-        {isLoading ? (
-          <div>Loading...</div>
-        ) : (
-          <div className="table-container">
-            <table>
-              <tbody>
-                {cardetails.map((element) => (
-                  <div className="table-cont">
-                    <tr key={element.id}>
-                      <td>
-                        <strong>Model:</strong>
-                      </td>
-                      <td>{element.model}</td>
-                    </tr>
-                    <tr key={`${element.id}-price`}>
-                      <td>
-                        <strong>Price:</strong>
-                      </td>
-                      <td>{element.price}</td>
-                    </tr>
-                    <tr key={`${element.id}-description`}>
-                      <td>
-                        <strong>Description:</strong>
-                      </td>
-                      <td>{element.description}</td>
-                    </tr>
-                    <tr key={`${element.id}-created_at`}>
-                      <td>
-                        <strong>Time Made:</strong>
-                      </td>
-                      <td>{element.created_at}</td>
-                    </tr>
-                  </div>
-                ))}
-              </tbody>
-            </table>
+      <div id={id}>
+        {cardetails.map((element) => (
+          <div className="details-div" key={element.id}>
+
+            <img src={element.image} alt="stadium" className="stadium" />
+            <div className="wrapper">
+              <div className="details">
+                <ul className="details-body">
+                  <div className="details-header">More details</div>
+                  <li className="details-body-item">
+                    Name:
+                    <span>{element.name}</span>
+                  </li>
+                  <li className="details-body-item">
+                    Price:
+                    <span>
+                      $
+                      {element.price}
+                    </span>
+                  </li>
+                  <li className="details-body-item">
+                    Model:
+                    <span>
+                      {element.model}
+                    </span>
+                  </li>
+                </ul>
+                {' '}
+
+              </div>
+              <div
+                className="discover"
+                onClick={handleClick}
+                onKeyDown={handleClick}
+                role="presentation"
+              >
+                discover more cars
+                <span
+                  onClick={handleClick}
+                  onKeyDown={handleClick}
+                  role="presentation"
+                >
+                  <BiChevronRight />
+                </span>
+              </div>
+              <img src={multicolor} alt="multicolor" className="multicolor" />
+              <NavLink
+                to="reserve"
+                className="buy"
+                id={id}
+              >
+                <span><CiSettings className="sett" /></span>
+                reserve
+              </NavLink>
+            </div>
           </div>
-        )}
+        ))}
+
       </div>
     </div>
+
   );
 };
 
